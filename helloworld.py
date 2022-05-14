6a9cfe973cc9c9695f39c3e4428db47672d8c728
raccoon75 <104978768+raccoon75@users.noreply.github.com>
2022-05-14 19:11:30 +0300

    Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -3,6 +3,7 @@ import pandas as pd
 import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
+import matplotlib.lines as mlines
 import altair as alt
 
 with st.echo(code_location='below'):
